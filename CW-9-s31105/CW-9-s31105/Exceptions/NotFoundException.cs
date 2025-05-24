5dc307ea563705232b7804ef985e125920494f4a
Marysia <s31105@pjwstk.edu.pl>
2025-05-24 21:34:54 +0200

    initial commit

---
+++ CW-9-s31105/CW-9-s31105/Exceptions/NotFoundException.cs
@@ -0,0 +1,4 @@
+namespace CW_9_s31105.Exceptions;
+
+public class NotFoundException(string message) : Exception(message);
+  
---
+++ CW-9-s31105/CW-9-s31105/Models/Medicament.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CW_9_s31105.Models;
+
+[Table [Medicament]]
+public class Medicament
+{
+    public int IdMedicament { get; set; }
+}
