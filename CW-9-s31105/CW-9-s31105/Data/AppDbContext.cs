4a56351c79e57d74c1075c8bb797e9d55611eb09
Marysia <s31105@pjwstk.edu.pl>
2025-05-25 22:33:41 +0200

    context commit

--- CW-9-s31105/CW-9-s31105/Data/AppDbContext.cs
+++ CW-9-s31105/CW-9-s31105/Data/AppDbContext.cs
@@ -19,6 +19,61 @@ public class AppDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+        var doctors = new List<Doctor>
+        {
+            new ()
+            {
+                IdDoctor = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "johndoe@gmail.com",
+            }
+        };
+        
+        var medicaments = new List<Medicament>
+        {
+            new ()
+            {
+                IdMedicament = 1,
+                Name = "Apap",
+                Description = "Przeciwbolowy",
+                Type = "Tabletki"
+            }
+        };
+        
+        var patients = new List<Patient>
+        {
+            new ()
+            {
+                IdPatient = 1,
+                FirstName = "Pola",
+                LastName = "Gorska",
+                BirthDate = DateTime.Now,
+            }
+        };
+        
+        var prescriptions = new List<Prescription>
+        {
+            new ()
+            {
+                IdPrescription = 1,
+                Date = DateTime.Now,
+                DueDate = DateTime.Now,
+                IdPatient = 1,
+                IdDoctor = 1,
+            }
+        };
+        
+        var prescriptionMedicaments = new List<Prescription_Medicament>
+        {
+            new ()
+            {
+                IdMedicament = 1,
+                IdPerscription = 1,
+                Dose = 4,
+                Details = "Przeciwbolowy",
+            }
+        };
         
         
     }
