2cda0a765f8304778172a98b254894b0d599bde8
Marysia <s31105@pjwstk.edu.pl>
2025-05-25 21:58:02 +0200

    Final commit 2

--- CW-9-s31105/CW-9-s31105/Models/Medicament.cs
+++ CW-9-s31105/CW-9-s31105/Models/Medicament.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CW_9_s31105.Models;
 
-[Table [Medicament]]
+[Table ("Medicament")]
 public class Medicament
 {
+    [Key]
     public int IdMedicament { get; set; }
+    [MaxLength(100)]
+    public String Name { get; set; }
+    [MaxLength(100)]
+    public String Description { get; set; }
+    [MaxLength(100)]
+    public String Type { get; set; }
+    public virtual ICollection<Prescription_Medicament> PrescriptionMedicaments { get; set; } = null!;
 }
